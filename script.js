abff3de4ed97ef69703ebe7ee6cf03ee3f92238d
Krimo0o <147518378+Krimo0o@users.noreply.github.com>
2023-10-22 19:16:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.scrollTo(0, 0); // Déplace la page vers le haut au chargement
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.scrollTo(0, 0);
+});
